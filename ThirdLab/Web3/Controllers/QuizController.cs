8cc800cb586519a248272ef3698f9ed1a4e15804
TheAffeqt <57591944+TheAffeqt@users.noreply.github.com>
2020-05-29 18:40:07 +0700

    Web3

---
+++ ThirdLab/Web3/Controllers/QuizController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab3.Models;
+
+namespace Lab3.Controllers
+{
+    public class QuizController : Controller
+    {
+        static QuizModel quiz = new QuizModel();
+        static Random rand = new Random();
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Quiz()
+        {
+            ViewBag.numb1 = rand.Next(10);
+            ViewBag.numb2 = rand.Next(10);
+            return View();
+        }
+
+        public IActionResult QuizResult()
+        {
+            ViewBag.ListResult = quiz.Results;
+            ViewBag.RightAnswersCount = quiz.rightAnswers;
+            ViewBag.AnswersCount = quiz.count;
+            return View();
+        }
+
+       public void QuizLogic (string value1, string value2, string answer)
+       {
+            int _value1 = Convert.ToInt32(value1);
+            int _value2 = Convert.ToInt32(value2);
+            int _answer = Convert.ToInt32(answer);
+
+            quiz.Results.Add(_value1 + " + " + _value2 + " = " + _answer);
+            quiz.count++;
+
+            if (_value1 + _value2 == _answer)
+            {
+                quiz.rightAnswers++;
+            }
+
+            ViewBag.numb1 = rand.Next(10);
+            ViewBag.numb2 = rand.Next(10);
+       }
+        
+        
+        [HttpPost]
+        public IActionResult Next(string value1, string value2, string answer)
+        {
+            QuizLogic(value1, value2, answer);
+            return View("quiz");
+        }
+
+        [HttpPost]
+        public IActionResult Finish(string value1, string value2, string answer)
+        {
+            QuizLogic(value1, value2, answer);
+            ViewBag.ListResult = quiz.Results;
+            ViewBag.RightAnswersCount = quiz.rightAnswers;
+            ViewBag.AnswersCount = quiz.count;
+            return View("quizResult");
+        }
+
+    }
+}
---
+++ ThirdLab/Web3/Models/QuizModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.Models
+{
+    public class QuizModel
+    {
+        public int answer { get; set; } = -1;
+        public int rightAnswers { get; set; } = 0;
+        public int count { get; set; } = 0;
+        public List<string> Results { get; set; } = new List<string>();
+
+        public QuizModel(string result, int rAnswer, int countAnswer)
+        {
+
+        }
+
+        public QuizModel()
+        {
+
+        }
+    }
+}
